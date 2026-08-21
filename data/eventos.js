de1cb217f0e7484427caf12824d8acf86cbbb968
frybroscotizacion-byte <frybroscotizacion@gmail.com>
2026-08-21 00:38:13 -0400

    Create eventos.js

---
+++ data/eventos.js
@@ -0,0 +1,8 @@
+const eventos = [
+  {
+    id: "evento-1",
+    imagen: "images/evento-1.jpg",
+    titulo: "Evento Fry Bros",
+    descripcion: "Una jornada preparando comida al momento para nuestros invitados."
+  }
+];

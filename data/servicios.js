d492e4e8cce765d7aae66d4ce3fb0f7b20af1d3d
frybroscotizacion-byte <frybroscotizacion@gmail.com>
2026-08-20 22:53:34 -0400

    Create servicios.js

---
+++ data/servicios.js
@@ -0,0 +1,41 @@
+const servicios = [
+  {
+    id: "papas-fritas",
+    nombre: "Papas Fritas",
+    imagen: "images/Papasfritas.jpeg",
+    descripcion: "Papas fritas recién preparadas para tu evento.",
+    pagina: "pages/papas-fritas.html"
+  },
+
+  {
+    id: "hamburguesas",
+    nombre: "Hamburguesas",
+    imagen: "images/Hamburguesa.jpeg",
+    descripcion: "Hamburguesas preparadas al momento para tus invitados.",
+    pagina: "pages/hamburguesas.html"
+  },
+
+  {
+    id: "hot-dogs",
+    nombre: "Hot Dogs",
+    imagen: "images/Hotdog.jpeg",
+    descripcion: "Hot dogs preparados directamente durante tu evento.",
+    pagina: "pages/hot-dogs.html"
+  },
+
+  {
+    id: "lomitos",
+    nombre: "Lomitos",
+    imagen: "images/lomito.jpeg",
+    descripcion: "Lomitos calientes preparados al momento.",
+    pagina: "pages/lomitos.html"
+  },
+
+  {
+    id: "churrascos",
+    nombre: "Churrascos",
+    imagen: "images/Churasco.jpeg",
+    descripcion: "Churrascos recién preparados para tus invitados.",
+    pagina: "pages/churrascos.html"
+  }
+];
